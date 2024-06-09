c492e9cc04cd9b2bb3af36a9dd98206a2d993adc
tapendu-technoexponent98 <tapendu@technoexponent.com>
2024-06-09 15:01:29 +0530

    working on validation and state management

---
+++ app/src/main/java/com/example/nuritopia_kotlin/Home.kt
@@ -0,0 +1,13 @@
+package com.example.nuritopia_kotlin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Home : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home)
+    }
+}
